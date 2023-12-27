e11f729d7745ec6d6dee366c3433beba5f8fbea6
emxanuel <ealg0528@gmail.com>
2023-12-27 02:58:01 -0400

    fix bug

--- src/components/FavSongs.tsx
+++ src/components/FavSongs.tsx
@@ -10,7 +10,7 @@ const FavSongs: React.FC<Props> = ({data}) => {
     return (
         <div className='grid grid-cols-2 gap-3 w-full place-items-stretch'>
             {
-                data.slice(0, 6).map((song, index) => (
+                data?.slice(0, 6).map((song, index) => (
                     <button key={index} className='flex gap-4 md:w-full w-full items-center bg-neutral-800 rounded-md hover:bg-neutral-700 duration-200'>
                         <div>
                             <Image
